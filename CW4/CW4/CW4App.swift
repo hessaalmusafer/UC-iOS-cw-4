ffcc32b3e5d73dc61ea729ad0240525813071e04
hessaalmusafer <hessaalmusafer@gmail.com>
2022-09-11 19:58:57 +0300

    /

---
+++ CW4/CW4/CW4App.swift
@@ -0,0 +1,17 @@
+//
+//  CW4App.swift
+//  CW4
+//
+//  Created by Hessa AlMusafer on 11/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct CW4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CW4/CW4/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CW4
+//
+//  Created by Hessa AlMusafer on 11/09/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
